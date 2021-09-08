83ddc1eb236f831808a72f42e83b86dca83f5bd1
Samantha Laine <samanthalaine8@gmail.com>
2021-09-08 16:56:10 -0400

    completed assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,16 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    include Dance
+    attr_accessor :name
+    extend MetaDancing
+    extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  
+    def initialize(name)
+      @name = name
+    end
+  end
+  
